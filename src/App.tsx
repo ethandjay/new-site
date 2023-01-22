8bd0bbdb6bf768153f34d7e028c8e67d74b013fc
Ethan Jaynes <ethandjay@Ethans-MacBook-Pro-2.local>
2023-01-22 15:59:30 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Ethan Jaynes</h1>
+      </header>
+      <section>
+        <p>
+          Brooklyn, NY<br/>
+          Software Engineer at <a href='https://www.olo.com/' target='_blank'>Olo</a>
+        </p>
+      </section>
+      <section>
+        <p>
+          <a href='mailto:ethandjay@gmail.com' target='_blank'>Contact</a><br/>
+          <a href='https://www.linkedin.com/in/ethanjaynes1/' target='_blank'>LinkedIn</a><br/>
+          <a href='https://twitter.com/ethan_jaynes' target='_blank'>Twitter</a>
+        </p>
+      </section>
+    </div>
+  )
+}
+
+export default App
